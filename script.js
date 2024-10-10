01cc0eede1f7f0825c1ca74e86acb9126a0098f2
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2024-10-10 19:07:11 +0000

    logic for 5x5 connect 5 @ 8 blocks

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
-var gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var currentTurn = 0;
 var tileLife = [[], []];
 var announcer;
+var gameRunning = true;
 
 document.addEventListener("DOMContentLoaded", (event) => {
     announcer = document.getElementById("announcer");
 })
 
+function checkWinningBoard(lastPlacedPiece) {
+    let sanitizedBoard = gameBoard.slice(0);
+    let hits = 0;
+    sanitizedBoard[sanitizedBoard.indexOf(3)] = 1;
+    sanitizedBoard[sanitizedBoard.indexOf(4)] = 2;
+
+    // modulus of 5 give the starting column position
+    hits = 0;
+
+    for (let checkIndex = lastPlacedPiece % 5; checkIndex < 25; checkIndex += 5) {
+        if (sanitizedBoard[checkIndex] == sanitizedBoard[lastPlacedPiece])
+            hits += 1;
+    }
+
+    if (hits == 5) return true;
+
+    // floor of division by 5 give starting row position
+    hits = 0;
+    let target = Math.floor(lastPlacedPiece / 5);
+
+    for (let checkIndex = target; checkIndex < target + 5; checkIndex++) {
+        if (sanitizedBoard[checkIndex] == sanitizedBoard[lastPlacedPiece])
+            hits += 1;
+    }
+
+    if (hits == 5) return true;
+
+    // modulus by 6 check if it is on the main diagonal
+    hits = 0;
+    
+    if (lastPlacedPiece % 6 == 0) {
+        for (let checkIndex = 0; checkIndex < 25; checkIndex += 6) {
+            if (sanitizedBoard[checkIndex] == sanitizedBoard[lastPlacedPiece])
+                hits += 1;
+        }
+
+        if (hits == 5) return true;
+    }
+
+    // modulus by 4 check if it is on the anti-diagonal
+    hits = 0;
+    
+    if (lastPlacedPiece % 4 == 0 && lastPlacedPiece != 0 && lastPlacedPiece != 24) {
+        for (let checkIndex = 4; checkIndex < 21; checkIndex += 4) {
+            if (sanitizedBoard[checkIndex] == sanitizedBoard[lastPlacedPiece])
+                hits += 1;
+        }
+
+        if (hits == 5) return true;
+    }
+
+    return false;
+}
+
 function renderGameBoard() {
-    for (let index = 0; index < 9; index++) {
-        let targetBlock = document.getElementById("box" + index);
+    for (let index = 0; index < 25; index++) {
+        let targetBlock = document.getElementById(index);
         switch (gameBoard[index]) {
             case 1:
                 targetBlock.className = "playerOneHighlight";
@@ -18,7 +73,7 @@ function renderGameBoard() {
             case 2:
                 targetBlock.className = "playerTwoHighlight";
                 break;
-            
+
             case 3:
                 targetBlock.className = "playerOneNextDisappear";
                 break;
@@ -26,7 +81,7 @@ function renderGameBoard() {
             case 4:
                 targetBlock.className = "playerTwoNextDisappear";
                 break;
-        
+
             default:
                 targetBlock.className = "";
                 break;
@@ -34,40 +89,45 @@ function renderGameBoard() {
     }
 }
 
-document.addEventListener("keydown", (event) => {
-    if (!isNaN(event.key)) {
-        let position = Number(event.key) - 1;
-        
-        // prevent duplicated entry
-        if (gameBoard[position] != 0 
-            && tileLife[currentTurn][tileLife[currentTurn].length - 1] != position) {
-            
-            announcer.innerHTML = "Chosen position is occupied!";
-            return;
-        }
+function handleBoardClick() {
+    if (!gameRunning) return;
+    let position = event.target.id;
 
-        // clear announcer
-        announcer.innerHTML = "";
+    // prevent duplicated entry
+    if (gameBoard[position] != 0
+        && tileLife[currentTurn][tileLife[currentTurn].length - 1] != position) {
 
-        // add corresponding entry
-        tileLife[currentTurn].unshift(position);
-
-
-        // update the gameboard
-        if (tileLife[currentTurn].length > 4) {
-            gameBoard[tileLife[currentTurn][3]] = 1 + currentTurn + 2;
-            gameBoard[tileLife[currentTurn].pop()] = 0;
-        }
-
-        gameBoard[position] = currentTurn + 1;
-
-        if (currentTurn == 0) {
-            currentTurn = 1;
-        }
-        else {
-            currentTurn = 0;
-        }
-
-        renderGameBoard();                
+        announcer.innerHTML = "Chosen position is occupied!";
+        return;
     }
-})
+
+    // clear announcer
+    announcer.innerHTML = "";
+
+    // add corresponding entry
+    tileLife[currentTurn].unshift(position);
+
+
+    // update the gameboard
+    if (tileLife[currentTurn].length > 8) {
+        gameBoard[tileLife[currentTurn].pop()] = 0;
+    }
+
+    gameBoard[tileLife[currentTurn][7]] = 1 + currentTurn + 2;
+    gameBoard[position] = currentTurn + 1;
+
+    renderGameBoard();
+    if (checkWinningBoard(position)) {
+        gameRunning = false;
+        announcer.innerHTML = "The winner is player " + (currentTurn + 1);
+    }
+
+    if (currentTurn == 0) {
+        currentTurn = 1;
+    }
+    else {
+        currentTurn = 0;
+    }
+
+}
+
